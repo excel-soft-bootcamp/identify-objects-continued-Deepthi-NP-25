dec6e7274472b8b9c5f533ddff736e2dd350b37c
Deepthi NP <65824317+Deepthi-NP-25@users.noreply.github.com>
2021-08-13 23:21:51 +0530

    Add files via upload

---
+++ DEMO/Student_info.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DEMO
+{
+    class Student_info
+    {
+        private string id;
+        private string name;
+        private string marks;
+        public string GetId()
+        {
+            return this.id;
+        }
+        public string GetName()
+        {
+            return this.name;
+        }
+        public string GetMarks()
+        {
+            return this.marks;
+        }
+        public void SetName(string value)
+        {
+            this.name = value;
+        }
+        public void SetID(string value)
+        { 
+            this.id = value;
+        }
+        public void SetMarks(string value)
+        { 
+            this.marks = value;
+        }
+    }
+}
