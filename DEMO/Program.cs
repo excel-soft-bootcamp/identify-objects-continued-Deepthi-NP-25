469d3f347a3f92a8ba3052e97d4cfd065cc4421f
Deepthi NP <65824317+Deepthi-NP-25@users.noreply.github.com>
2021-08-13 23:26:57 +0530

    Add files via upload

---
+++ DEMO/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DEMO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student_info _instance = new Student_info();
+            _instance.SetID("001");
+            _instance.SetName("Tom");
+            _instance.SetMarks("100");
+            string idValue = _instance.GetId();
+            string nameValue = _instance.GetName();
+            string marksValue = _instance.GetMarks();
+
+            System.Console.WriteLine(idValue);
+            System.Console.WriteLine(nameValue);
+            System.Console.WriteLine(marksValue);
+           
+        }
+    }
+}
